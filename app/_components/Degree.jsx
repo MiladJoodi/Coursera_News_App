42a7f62db1ab67b86f12c84254966170b481120b
Milad Joodi <winbeta.com@gmail.com>
2024-03-27 02:19:27 +0330

    Update Degree.jsx

--- app/_components/Degree.jsx
+++ app/_components/Degree.jsx
@@ -13,7 +13,7 @@ export default async function Degree() {
     return (
         <div className="px-8 min-w-[380px]">
             <h2 className="text-2xl font-bold mb-4">Earn Your Degree</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
                 {unis.map((uni, index) => index < 4 && (
                     <Card className="w-full flex flex-col gap-3" key={uni.id}>
                         <Image
@@ -28,18 +28,18 @@ export default async function Degree() {
                         />
                         <CardContent>
                             <div className="flex justify-between">
-                                <div className="flex items-start space-x-2 mb-6 max-w-[80%]">
-                                    <Avatar>
+                                <div className="flex items-start space-x-1 sm:space-x-2 mb-6 max-w-[80%]">
+                                    <Avatar className="hidden sm:flex">
                                         <AvatarImage alt="University of Michigan" src="/placeholder.svg?height=24&width=24" />
                                         <AvatarFallback>U</AvatarFallback>
                                     </Avatar>
                                     <div className="flex flex-col gap-2">
                                         <div className="font-semibold nowrap overflow-hidden text-ellipsis max-w-[250px] truncate">{uni.title}</div>
-                                        <div className="text-sm text-gray-500 line-clamp-3">{uni.desc}</div>
+                                        <div className="text-sm text-gray-500 line-clamp-3 text-justify">{uni.desc}</div>
                                     </div>
                                 </div>
 
-                                <div className="flex gap-2 pt-2">
+                                <div className="flex flex-col sm:flex-row gap-2 pt-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-600 cursor-pointer">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                     </svg>
@@ -56,10 +56,17 @@ export default async function Degree() {
                                 </svg>
                                 Earn a degree
                             </Button>
-                            <div className="text-sm text-gray-500 line-clamp-1">
-                                
-                                {uni.address}
+                            <div className="text-sm text-gray-500 line-clamp-1 flex gap-1">
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                    </svg>
                                 </div>
+                                <div className="line-clamp-1">
+                                    {uni.address}
+                                </div>
+                            </div>
                         </CardContent>
                     </Card>
                 ))}
