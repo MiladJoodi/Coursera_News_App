22a15c12db7dd01d0e80569b5b18deed9791b2c8
Milad Joodi <winbeta.com@gmail.com>
2024-03-27 20:16:36 +0330

    ty

--- app/details/[slug]/page.jsx
+++ app/details/[slug]/page.jsx
@@ -1,14 +1,16 @@
-
-import SimpleSlider from "@/app/_components/Slider/Slider";
+import Slider from "@/app/_components/Slider/Slider";
 import { unis } from "@/lib/data";
+import Autoplay from 'embla-carousel-autoplay'
 
 
 const Details = ({ params }) => {
+    const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
+
     console.log({ params })
     return (
         <div className="flex flex-col px-4 text-l">
             <h2 className="text-xl font-bold col-span-full py-2">{params.slug}</h2>
-            <SimpleSlider />
+            <Slider />
         </div>
     );
 }
