5695355c933c0f96fdf2be024dfec6b1b06cf36f
Milad Joodi <winbeta.com@gmail.com>
2024-03-27 21:27:57 +0330

    up

--- app/_components/Slider/Slider.jsx
+++ app/_components/Slider/Slider.jsx
@@ -1,19 +1,58 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
+import { unis } from '@/lib/data'
+import Image from 'next/image'
 
 export default function Slider() {
-  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()])
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
+    <div className='embla rounded-xl'>
+      <div className="embla__viewpor h-56 max-w-full rounded-xl" ref={emblaRef}>
+        <div className="embla__container">
+          {unis.map((uni, index)=>{
+            {uni.images.map((item, i)=> {
+              return (
+                <div className="embla__slide">
+                {console.log(item)}
+                {/* <Image
+                        src="/logo.png"
+                        width={160}
+                        height={70}
+                        alt="logo"
+                        style={{
+                            aspectRatio: "150/55",
+                        }}
+                    /> */}
+              </div>
+              )
+            })}
+          })}
+          {/* <div className="embla__slide">Slide 1</div> */}
+          {/* <div className="embla__slide">Slide 2</div> */}
+          {/* <div className="embla__slide">Slide 3</div> */}
+        </div>
       </div>
+
+      {/* Buttons */}
+      <button className="embla__prev" onClick={scrollPrev}>
+        Prev
+      </button>
+      <button className="embla__next" onClick={scrollNext}>
+        Next
+      </button>
+
     </div>
   )
 }
